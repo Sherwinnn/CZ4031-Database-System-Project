c3533d9b086dce02d2ba2278e847a557044d58c8
fion-chai <99871669+fion-chai@users.noreply.github.com>
2022-10-02 14:04:51 +0800

    changed group number

--- app/Constants.java
+++ app/Constants.java
@@ -1,7 +1,7 @@
 package app;
 
 public interface Constants {
-    int GROUP_NUM = 4;
+    int GROUP_NUM = 21;
     int DISK_SIZE = 200*1000*1000; // 200 MB
     int BLOCK_200 = 200;
     int BLOCK_500 = 500;
