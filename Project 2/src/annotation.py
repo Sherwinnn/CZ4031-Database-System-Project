9aa96869bdd3f34d1dc66af83a7ab09cd79843f1
Sherwinnn <sherwinsamson1@hotmail.com>
2022-11-04 14:34:45 +0800

    test

---
+++ Project 2/src/annotation.py
@@ -0,0 +1,258 @@
+class Annotator:
+
+    def __init__(self):
+        self.iCount = 0
+        self.stepCount = 0
+
+    # wrapper function to do preprocessing on the qep and to only return finished string
+    def wrapper(self, qep):
+        # just to make it a bit nicer
+        final = self.annotate(qep[0][0][0]['Plan'], True)[1]
+        final = final[:-3]
+        final += " to get the final result."
+        return final 
+
+    def annotate(self, query, first = False):
+        
+        # for storing previous tables since they are not included in the qep
+        joinTables = []
+
+        # result string to be combined with current iter's output and returned
+        result = ""
+
+        if "Plans" in query:
+            for plan in query["Plans"]:
+                temp = self.annotate(plan)
+                joinTables.append(temp[0])
+                result += temp[1]
+
+
+        self.stepCount += 1
+        result += "Step {}: ".format(self.stepCount)
+
+        # python 3.8 has no switch function :(
+
+        if query["Node Type"] == 'Seq Scan':
+            table = query["Relation Name"]
+            name = query["Alias"]
+            annie = "Perform sequential scan on table {} as {}".format(table, name)
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            annie += ". \n"
+            return table, result + annie
+
+
+        elif query["Node Type"] == 'Index Scan':
+            table = query["Relation Name"]
+            name = query["Alias"]
+            annie = "Perform index scan on table {} as {} using index on {}".format(table, name, query["Index Name"])
+            if "Index Cond" in query:
+                annie += " where {}".format(query["Index Cond"])
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            annie += ". \n"
+            return table, result + annie
+
+
+        elif query["Node Type"] == 'Index-Only Scan':
+            table = query["Relation Name"]
+            name = query["Alias"]
+            annie = "Perform index scan on table {} as {} using index on {}".format(table, name, query["Index Name"])
+            if "Index Cond" in query:
+                annie += " where {}".format(query["Index Cond"])
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            annie += ". \n"
+            return table, result + annie
+
+
+        elif query["Node Type"] == 'CTE Scan':
+            table = query["CTE Name"]
+            name = query["Alias"]
+            annie = "Perform CTE scan on table {} as {}".format(table, name)
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            annie += ". \n"
+            return table, result + annie
+
+
+        elif query["Node Type"] == 'Foreign Scan':
+            table = query["Relation Name"]
+            name = query["Alias"]
+            annie = "Perform foreign scan on table {} from schema {} as {}. \n".format(table, query["Schema"], name)
+            return table, result + annie
+
+        
+        elif query["Node Type"] == 'Function Scan':
+            table = query["Schema"]
+            name = query["alias"]
+            annie = "Perform function {} on schema {} and return the results as {}".format(query["Function Name"], table, name)
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            annie += ". \n"
+            return table, result + annie
+
+        
+        elif query["Node Type"] == 'Subquery Scan':
+            annie = "The subquery results from the previous operation is read"
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            annie += ". \n"
+            return joinTables[0], result + annie
+
+        
+        elif query["Node Type"] == 'TID Scan':
+            table = query["Relation"]
+            name = query["Alias"]
+            annie = "Perform a Tuple ID scan on table {} as {}. \n".format(table ,name)
+            return table, result + annie
+
+        
+        elif query["Node Type"] == 'Nested Loop':
+            self.iCount += 1
+            annie = "Perform a nested loop join on tables {} and {}".format(joinTables[0], joinTables[1])
+            if "Join Filter" in query:
+                annie += " under the condition {}".format(joinTables[0], joinTables[1], query["Join Filter"])
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+        
+        elif query["Node Type"] == 'Hash Join':
+            self.iCount += 1
+            annie = "Perform a hash join on tables {} and {}".format(joinTables[0], joinTables[1])
+            if "Hash Cond" in query:
+                annie += " under the condition {}".format(query["Hash Cond"])
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+
+        elif query["Node Type"] == 'Merge Join':
+            self.iCount += 1
+            annie = "Perform a merge join on tables {} and {}".format(joinTables[0], joinTables[1])
+            if "Merge Cond" in query:
+                annie += " under the condition {}".format(query["Merge Cond"])
+            if "Filter" in query:
+                annie += " with filter {}".format(query["Filter"])
+            annie += ". \n"
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+        
+        elif query["Node Type"] == 'Aggregate':
+            self.iCount += 1
+            annie = "Perform aggregate on table {}".format(joinTables[0])
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+        
+        elif query["Node Type"] == 'Append':
+            self.iCount += 1
+            annie = "Append the results from table {} to table {}".format(joinTables[0], joinTables[1]) 
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+        
+        elif query["Node Type"] == 'Gather':
+            self.iCount += 1
+            annie = ("Perform gather on table {}".format(joinTables[0]))
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+        
+        elif query["Node Type"] == 'Gather Merge':
+            annie = "The results of the previous operation are gathered and merged. \n"
+            return joinTables[0], result + annie
+
+        
+        elif query["Node Type"] == 'GroupAggregate':
+            self.iCount += 1
+            annie = "Perform a group aggregate on table {}".format(joinTables[0])
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+        
+        elif query["Node Type"] == 'Hash':
+            annie = "Perform hashing on table {}. \n".format(joinTables[0])
+            return joinTables[0], result + annie
+
+
+        elif query["Node Type"] == 'HashAggregate':
+            self.iCount += 1
+            annie = "Perform a hash aggregate on table {}".format(joinTables[0])
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+
+        elif query["Node Type"] == 'Incremental Sort':
+            annie = "An incremetal sort is performed on table {} with sort key {}. \n".format(joinTables[0], query["Sort Key"])
+            return joinTables[0], result + annie
+
+
+        elif query["Node Type"] == 'Limit':
+            annie = "The specified number of rows is selected from table {}. \n".format(joinTables[0])
+            return joinTables[0], result + annie
+
+
+        elif query["Node Type"] == 'Materialize':
+            annie = "Materialize table {}. \n".format(joinTables[0])
+            return joinTables[0], result + annie
+
+
+        elif query["Node Type"] == 'MergeAppend':
+            self.iCount += 1
+            annie = "Results from table {} are appended to table {}".format(joinTables[0], joinTables[1])
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+
+        elif query["Node Type"] == 'ModifyTable':
+            table = query["Relation Name"]
+            annie = "Table {} is modified. \n ".format(table)
+            return table, result + annie
+
+
+        elif query["Node Type"] == 'SetOp':
+            self.iCount += 1
+            annie = "A set operation is performed on table {}".format(joinTables[0])
+            if not first:
+                annie += " to get intermediate table T{}. \n".format(self.iCount)
+            else: annie += ". \n"
+            return "T" + str(self.iCount), result + annie
+
+
+        elif query["Node Type"] == 'Sort':
+            annie = "Perform a sort on table {} with sort key {}. \n".format(joinTables[0], query["Sort Key"])
+            return joinTables[0], result + annie
+
+
+        elif query["Node Type"] == 'Unique':
+            table = query["Subplan Name"] if "Subplan Name" in query else joinTables[0]
+            annie = "Duplicates are removed from table {}. \n".format(table)
+            return table, result + annie
+
+
+        else:
+            annie = "Perform {}. \n".format(query["Node Type"])
+            return joinTables[0], result + annie
---
+++ Project 2/src/project.py
@@ -0,0 +1,120 @@
+import sys 
+import time
+import json
+import psycopg2 # need install
+from PyQt5.QtWidgets import QApplication # need install
+from qt_material import apply_stylesheet #, list_themes # need install
+from interface import *
+from annotation import *
+
+# extract hard coded values
+FILE_CONFIG = "config.json"
+FILE_APP_THEME = "light_cyan_500.xml" #list_themes()[12]
+
+
+# allow use of with syntax
+class DatabaseCursor(object):
+    def __init__(self, config):
+        self.config = config
+
+    def __enter__(self):
+        self.conn = psycopg2.connect(
+            host=self.config["host"],
+            dbname=self.config["dbname"],
+            user=self.config["user"],
+            password=self.config["pwd"],
+            port=self.config["port"]
+        )   
+        self.cur = self.conn.cursor()
+        #self.cur.execute("SET search_path TO " + self.config['schema'])
+        return self.cur
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # some logic to commit/rollback
+        self.conn.close()
+
+class Program():
+    def __init__(self):
+        with open(FILE_CONFIG, "r") as file:
+            self.config = json.load(file)
+        # init ui components
+        self.app = QApplication(sys.argv)
+        apply_stylesheet(self.app, theme=FILE_APP_THEME)
+        self.window = UI()
+        self.window.setOnDatabaseChanged( lambda: self.onDatabaseChanged())
+        self.window.setOnAnalyseClicked( lambda: self.analyseQuery() )
+
+    def run(self):
+        self.window.show()
+        list_db = list(self.config.keys() )
+        print(f"List of database configs from json file: {list_db}")
+        self.window.setListDatabase(list_db)
+        sys.exit(self.app.exec_())
+        
+    def onDatabaseChanged(self):
+        # check cur database, update schema?
+        cur_db = self.window.list_database.currentText()
+        print(f"Current selected database is {cur_db}")
+        self.db_config = self.config[cur_db]
+        self.updateSchema()
+
+    def hasDbConfig(self):
+        if not hasattr(self, "db_config"): 
+            return False
+        if self.db_config == None:
+            return False
+        return True
+
+    def analyseQuery(self):
+        if not self.hasDbConfig():
+            self.window.showError("Database configuration is not found")
+            return
+        try:
+            query = self.window.readInput()
+            if not query:
+                print("query is empty")
+                return
+            print("query: \n%s"%query)
+            with DatabaseCursor(self.db_config) as cursor:
+                cursor.execute("EXPLAIN (FORMAT JSON) " + query)
+                plan = cursor.fetchall()
+                print("qep: \n%s"%plan)
+                plan_annotated = Annotator().wrapper(plan)
+                print("annotated qep: \n%s"%plan_annotated)
+                self.window.setResult( plan_annotated )
+                
+        except Exception as e:
+            print(str(e))
+            self.window.showError("Unable to analyse query!", e)
+
+    def updateSchema(self):
+        if not self.hasDbConfig(): 
+            self.window.setSchema(None)
+            self.window.showError("Database configuration is not found")
+            return
+        try:
+            with DatabaseCursor(self.db_config) as cursor:
+                query = "SELECT table_name, column_name, data_type, character_maximum_length as length FROM information_schema.columns WHERE table_schema='public' ORDER BY table_name, ordinal_position"
+                cursor.execute(query)
+                response = cursor.fetchall()
+
+                # parse response as dictionary 
+                schema = {}
+                for item in response:
+                    # cols are table_name, column_name, data_type, length (nullable)
+                    attrs = schema.get(item[0], [])
+                    attrs.append(item[1])
+                    schema[item[0]] = attrs
+                # log schema
+                print("Database schema as follow: ")
+                for t, table in enumerate(schema):
+                    print(t+1, table, schema.get(table))
+                
+                self.window.setSchema(schema)
+        except Exception as e:
+            print(str(e))
+            self.window.showError("Unable to retrieve schema information!", e)
+    
+    
+if __name__ == "__main__":
+    Program().run()
